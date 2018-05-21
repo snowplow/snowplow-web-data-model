f9e925bf9e6151ed300141f124361845e22bb77a
Dilyan Damyanov <dilyand@gmail.com>
2018-05-21 11:39:27 +0100

    Add 00-setup/00-setup-manifest.sql

---
+++ incremental/00-setup/00-setup-manifest.sql
@@ -0,0 +1,10 @@
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS scratch.manifest ( -- change to derived.manifest
+	etl_tstamp TIMESTAMP ENCODE ZSTD,
+  CONSTRAINT scratch_etl_tstamps_pk PRIMARY KEY(etl_tstamp)
+)
+DISTSTYLE ALL
+SORTKEY(etl_tstamp);
+
+-- Seed the manifest table
+INSERT INTO scratch.manifest(etl_tstamp) VALUES(GETDATE() - INTERVAL '1 week') -- change to derived.manifest and {{.start_date}}::TIMESTAMP
