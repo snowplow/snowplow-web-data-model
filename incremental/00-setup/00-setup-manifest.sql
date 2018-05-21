8176b06413cf9e7f9120e886d2486efe0fb82290
Dilyan Damyanov <dilyand@gmail.com>
2018-05-21 14:43:33 +0100

    Fix typo

--- incremental/00-setup/00-setup-manifest.sql
+++ incremental/00-setup/00-setup-manifest.sql
@@ -1,7 +1,7 @@
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS scratch.manifest ( -- change to derived.manifest
 	etl_tstamp TIMESTAMP ENCODE ZSTD,
-  CONSTRAINT scratch_etl_tstamps_pk PRIMARY KEY(etl_tstamp)
+  CONSTRAINT scratch_manifest_pk PRIMARY KEY(etl_tstamp)
 )
 DISTSTYLE ALL
 SORTKEY(etl_tstamp);
