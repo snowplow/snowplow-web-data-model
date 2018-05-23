996625fc9907681ef86f7edbf75d96334c57c1a5
Dilyan Damyanov <dilyand@gmail.com>
2018-05-23 15:31:49 +0100

    Make templateable

--- sql-runner/sql/incremental-web-model/00-setup/00-setup-manifest.sql
+++ sql-runner/sql/incremental-web-model/00-setup/00-setup-manifest.sql
@@ -1,19 +1,19 @@
 -- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS scratch.manifest ( -- change to derived.manifest
+CREATE TABLE IF NOT EXISTS {{.output_schema}}.manifest (
 	etl_tstamp TIMESTAMP ENCODE ZSTD,
-  CONSTRAINT scratch_manifest_pk PRIMARY KEY(etl_tstamp)
+  CONSTRAINT {{.output_schema}}_manifest_pk PRIMARY KEY(etl_tstamp)
 )
 DISTSTYLE ALL
 SORTKEY(etl_tstamp);
 
 -- Seed the manifest table
-INSERT INTO scratch.manifest(etl_tstamp) ( -- change to derived.manifest
+INSERT INTO {{.output_schema}}.manifest(etl_tstamp) (
 
   SELECT
     MIN(etl_tstamp)
   FROM
-    atomic.events
+    {{.input_schema}}.events
   WHERE
-    DATE_TRUNC('day', etl_tstamp) = DATE_TRUNC('day', GETDATE() - INTERVAL '1 week') -- replace GETDATE() - INTERVAL '1 week' with {{.start_date}}::TIMESTAMP
+    DATE_TRUNC('day', etl_tstamp) = DATE_TRUNC('day', '{{.start_date}}'::TIMESTAMP)
 
 );
