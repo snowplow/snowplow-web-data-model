8f602017516a87cc1261188b6b17c4cfa95d517b
Dilyan Damyanov <dilyand@gmail.com>
2018-07-04 15:19:37 +0100

    Update sql/incremental-web-model/01-page-views/02-select-event-ids.sql

--- incremental/sql-runner/sql/incremental-web-model/01-page-views/02-select-event-ids.sql
+++ incremental/sql-runner/sql/incremental-web-model/01-page-views/02-select-event-ids.sql
@@ -5,6 +5,7 @@
 CREATE TABLE IF NOT EXISTS {{.scratch_schema}}.event_ids (
 	event_id CHAR(36) ENCODE ZSTD,
 	collector_tstamp TIMESTAMP ENCODE ZSTD,
+  domain_sessionid CHAR(128) ENCODE ZSTD,
   CONSTRAINT {{.scratch_schema}}_event_ids_pk PRIMARY KEY(event_id)
 )
 DISTKEY(event_id)
@@ -17,17 +18,18 @@ TRUNCATE {{.scratch_schema}}.event_ids;
 -- 2c. insert all event ID and timestamp that have not been processed
 
 INSERT INTO {{.scratch_schema}}.event_ids (
-
 	SELECT
 		event_id,
-		collector_tstamp
+		collector_tstamp,
+    domain_sessionid
 	FROM
     {{.input_schema}}.events
 	WHERE
     etl_tstamp IN (SELECT * FROM {{.scratch_schema}}.etl_tstamps)
 		AND event_name = 'page_view' -- restrict to page views for which we have the initial page view event
     AND collector_tstamp > (SELECT MIN(etl_tstamp) FROM {{.scratch_schema}}.etl_tstamps) - INTERVAL '1 week' -- for performance, but also excludes timestamp outliers
-  GROUP BY 1, 2 -- in case of any duplicates
+    AND collector_tstamp >= '{{.start_date}}'::TIMESTAMP -- excludes timestamp outliers
+    AND derived_tstamp >= '{{.start_date}}'::TIMESTAMP -- excludes timestamp outliers
+  GROUP BY 1, 2, 3 -- in case of any duplicates
   ORDER BY 1
-
 );
