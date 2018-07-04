766f8406e1282906dcabcf7d3beeff1fe19cb7bc
Dilyan Damyanov <dilyand@gmail.com>
2018-07-04 15:38:47 +0100

    Update sql/incremental-web-model/01-page-views/09-delete-page-views.sql

--- incremental/sql-runner/sql/incremental-web-model/01-page-views/09-delete-page-views.sql
+++
@@ -1,5 +0,0 @@
--- 9. DELETE PAGE VIEWS
-
-/* This step only deletes events for which we have recomputed data. The new data will be inserted in the next step. */
-
-DELETE FROM {{.output_schema}}.page_views WHERE page_view_id IN (SELECT page_view_id FROM {{.scratch_schema}}.page_views);
