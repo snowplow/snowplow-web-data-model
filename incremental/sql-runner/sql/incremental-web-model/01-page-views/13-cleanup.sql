cae3b89e946641424fee1f15d7af04c5627876ed
Dilyan Damyanov <dilyand@gmail.com>
2018-07-04 15:41:22 +0100

    Update sql/incremental-web-model/01-page-views/12-cleanup.sql

--- incremental/sql-runner/sql/incremental-web-model/01-page-views/13-cleanup.sql
+++ incremental/sql-runner/sql/incremental-web-model/01-page-views/13-cleanup.sql
@@ -1,4 +1,4 @@
--- 12. CLEAN UP
+-- 13. CLEAN UP
 
 TRUNCATE {{.scratch_schema}}.etl_tstamps; -- step 1
 TRUNCATE {{.scratch_schema}}.event_ids; -- step 2
@@ -7,4 +7,5 @@ TRUNCATE {{.scratch_schema}}.ids; -- step 4
 TRUNCATE {{.scratch_schema}}.page_view_events; -- step 5
 TRUNCATE {{.scratch_schema}}.page_view_time; -- step 6
 TRUNCATE {{.scratch_schema}}.page_view_rank; -- step 7
-TRUNCATE {{.scratch_schema}}.page_views; -- step 8
+TRUNCATE {{.scratch_schema}}.events_scroll_depth; -- step 8
+TRUNCATE {{.scratch_schema}}.page_views; -- step 9
