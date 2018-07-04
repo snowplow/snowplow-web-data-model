66b8a24914ec9265d955758a5e8159fe793f5d89
Dilyan Damyanov <dilyand@gmail.com>
2018-07-04 15:18:04 +0100

    Update sql/incremental-web-model/01-page-views/01-select-etl-tstamps.sql

--- incremental/sql-runner/sql/incremental-web-model/01-page-views/01-select-etl-tstamps.sql
+++ incremental/sql-runner/sql/incremental-web-model/01-page-views/01-select-etl-tstamps.sql
@@ -16,18 +16,17 @@ TRUNCATE {{.scratch_schema}}.etl_tstamps;
 -- 1c. insert all ETL timestamps that are not in the manifest (i.e. have not been processed)
 
 INSERT INTO {{.scratch_schema}}.etl_tstamps (
-
   SELECT
     etl_tstamp
 	FROM
     {{.input_schema}}.events
   WHERE
-    etl_tstamp NOT IN (SELECT etl_tstamp FROM {{.output_schema}}.manifest ORDER BY 1)
+    etl_tstamp NOT IN (SELECT etl_tstamp FROM {{.output_schema}}.page_views_manifest ORDER BY 1)
 		AND etl_tstamp BETWEEN
-      (SELECT MAX(etl_tstamp) FROM {{.output_schema}}.manifest) - INTERVAL '1 week' -- to ensure that any quarantined data is not missed
+      (SELECT MAX(etl_tstamp) FROM {{.output_schema}}.page_views_manifest) - INTERVAL '1 week' -- to ensure that any quarantined data is not missed
       AND
-      (SELECT MAX(etl_tstamp) FROM {{.output_schema}}.manifest) + INTERVAL '{{.update_cadence}}'
+      (SELECT MAX(etl_tstamp) FROM {{.output_schema}}.page_views_manifest) + INTERVAL '{{.update_cadence}}'
+    AND etl_tstamp > '{{.start_date}}'
   GROUP BY 1
   ORDER BY 1
-
 );
